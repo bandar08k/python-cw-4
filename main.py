f0a63f3ea6cfea8e95800cf7d82d9762e062b180
bandar08k <bandar08k@gmail.com>
2023-09-02 20:02:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def my_name():
+    print("My name is Bandar")
+
+my_name()
+
+def my_meal(food, drink):
+    
+    print(f"I like to eat {food} and while drinking {drink}")
+
+food = ('Burger')
+drink = ('Pepsi')
+
+my_meal(food,drink)
